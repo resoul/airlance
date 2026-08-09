df5c681737f4c34349971cab49873687c89996f5
resoul <resoul@bk.ru>
2026-08-09 17:50:21 +0300

    prepare for services

--- internal/transport/http/utils/response.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-type ErrorResponse struct {
-	Error   string                 `json:"error"`
-	Message string                 `json:"message,omitempty"`
-	Details map[string]interface{} `json:"details,omitempty"`
-}
--- internal/utilities/version.go
+++
@@ -1,4 +0,0 @@
-package utilities
-
-// Version is git commit or release tag from which this binary was built.
-var Version string
